e2b4a96f343d868270770eae18d6883f42c20828
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-26 16:25:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,8 +58,8 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  num = gets.chomp 
-  index = input_to_index(num)
+  user_input = gets.chomp 
+  index = input_to_index(user_input)
   if valid_move?(board, index) 
     player_token = current_player(board)
     move(board, index, player_token)
